68dcdc71e2d0008a133d319f2ab08b4728b6d97b
miradofitiavana <miradofitiavana@gmail.com>
2022-03-10 12:18:03 +0100

    feat : changer font global

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import {SharedModule} from "../../shared/shared.module";
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  }
+];
 
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
+    RouterModule.forChild(routes),
     SharedModule
   ]
 })
